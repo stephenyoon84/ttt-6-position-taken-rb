4d432347b841607fb24a41ecedf9743fb8da3ea9
stephenyoon84 <stephenyoon84@gmail.com>
2019-02-08 10:41:52 -0500

    add #position_taken?

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  case board[index]
+  when " "
+    return false
+  when ""
+    return false
+  when nil
+    return false
+  when "X"
+    return true
+  when "O"
+    return true
+  end
+
+end
